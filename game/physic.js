df5e80d5a99ef2c51b3e94d0ffde1cdd6479a9b8
Arthur <57642175+Tankadaire@users.noreply.github.com>
2021-09-22 19:44:35 +0200

    test rotation

--- game/physic.js
+++ game/physic.js
@@ -2,12 +2,13 @@ function update()
 {
     var delta = clock.getDelta(); // seconds.
     var moveDistance = 50 * delta; // 200 pixels per second
-    var rotateAngle = Math.PI / 2 * delta * 2;   // pi/2 radians (90 degrees) per second
+    var rotateAngleleft = Math.PI / 2 * delta * 2;   // pi/2 radians (90 degrees) per second
+	var rotateAngleright = Math.PI / 2 * delta * -(2);
 
     if (keyboard.pressed("left"))
-        player1.turnLeft(rotateAngle);
+        player1.turnLeft(rotateAngleleft);
     if (keyboard.pressed("right"))
-        player1.turnRight(rotateAngle);
+        player1.turnRight(rotateAngleright);
     if (keyboard.pressed("up"))
         player1.accelerate(moveDistance);
     if (keyboard.pressed("down"))
